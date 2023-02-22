95bca642ca6e78d0895f63b9f2f212da8d17b066
Faberson Perfeito <fabersonperfeito@gmail.com>
2023-02-22 18:21:11 -0300

    Initial Commit

---
+++ TesteGithub/ContentView.swift
@@ -0,0 +1,28 @@
+//
+// perfeito.dev
+// PIX: perfeito.dev@gmail.com
+//
+// ContentView.swift
+// TesteGithub
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TesteGithub/TesteGithubApp.swift
@@ -0,0 +1,19 @@
+//
+// perfeito.dev
+// PIX: perfeito.dev@gmail.com
+//
+// TesteGithubApp.swift
+// TesteGithub
+//
+
+
+import SwiftUI
+
+@main
+struct TesteGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
