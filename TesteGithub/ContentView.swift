143cc1577a88e0735e0ab4c2fc452a21b6b8a423
Faberson Perfeito <fabersonperfeito@gmail.com>
2023-02-22 18:25:32 -0300

    Change text Label

--- TesteGithub/ContentView.swift
+++ TesteGithub/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello World")
         }
         .padding()
     }
